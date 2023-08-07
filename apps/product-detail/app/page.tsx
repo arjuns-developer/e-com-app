3c1a4f0c60d569c8f099c0774e5af7b81ef1b802
arjun.surendran <arjun.surendran@qburst.com>
2023-08-07 21:12:01 +0530

    initial setup

---
+++ apps/product-detail/app/page.tsx
@@ -0,0 +1,14 @@
+import styles from './page.module.css';
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <>
+      <h1>Hello there, Welcome product-detail ffff 👋</h1>
+    </>
+  );
+}
